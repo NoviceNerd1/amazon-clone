6adf5a382ee58160355479940ef53cde1d9848c9
NoviceNerd1 <samarathay123@gmail.com>
2024-08-22 01:32:04 +0530

    started making home page

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
