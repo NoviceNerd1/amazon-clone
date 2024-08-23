e22c55b2e15751fadde13c2ca6dca126b6eca6fb
NoviceNerd1 <samarathay123@gmail.com>
2024-08-23 23:00:31 +0530

    Updating home page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -7,9 +8,22 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
+
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   );
