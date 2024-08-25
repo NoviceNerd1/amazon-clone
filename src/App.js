da3b8fb14cb9ef42785b905b3023ab73a0b303a7
NoviceNerd1 <samarathay123@gmail.com>
2024-08-26 00:46:33 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    // BEM
+      // BEM
+    <Router>
     <div className="app">
       <Header />
       <Home />
-    </div>
+          </div>
+      </Router>
   );
 }
 
